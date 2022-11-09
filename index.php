18b6a795950ed36a53f7f661f7410d675f6c6b11
Timi Havran <havran.timea@gmail.com>
2022-11-09 13:19:04 +0000

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 	<head>
 		<meta charset="utf-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
-		<title>Oak Stave Craft</title>
+		<title>Name of the company</title>
 
 		<link rel="stylesheet" href="css/style.css">
 		<link rel="stylesheet" href="css/bootstrap-5.2.0/dist/css/bootstrap.min.css">
